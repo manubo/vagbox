788686a0708e3c50e054f304909805d06812cea5
Manuel Bouza <manuel@bouza.ch>
2014-04-27 15:28:33 +0200

    folder www renamed to httpd

--- cookbooks/symfony2/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-execute "install_symfony2" do
-	cwd "/srv/www"
-  	command "rm -rf * && find ./ -iname \".*\" -type f -exec rm -rf {} \\; && composer create-project symfony/framework-standard-edition /srv/httpd/#{node[:sys][:name]}"
-  	creates "/srv/www/web/app.php"
-end
