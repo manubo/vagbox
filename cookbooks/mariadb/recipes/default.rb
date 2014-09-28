091db5437c0b7e9fb6310c76841c3b6bdf8777c0
Manuel Bouza <manuel@bouza.ch>
2014-09-28 17:06:49 +0200

    improved mariadb config

--- cookbooks/mariadb/recipes/default.rb
+++ cookbooks/mariadb/recipes/default.rb
@@ -4,3 +4,8 @@ include_recipe "mariadb::config"
 package "mariadb-server" do
   action :install
 end
+
+service "mysql" do
+  supports :status => true, :restart => true, "force-reload" => true
+  action [:enable, :start]
+end
--- cookbooks/mariadb/recipes/galera.rb
+++ cookbooks/mariadb/recipes/galera.rb
@@ -27,3 +27,20 @@ end
 package "rsync" do
 	action :install
 end
+
+if (node[:mariadb][:is_first])
+
+  service "mysql" do
+    supports :status => true, :restart => true, "force-reload" => true
+    start_command "/etc/init.d/mysql start --wsrep-new-cluster"
+    action [:enable, :start]
+  end
+
+else
+
+  service "mysql" do
+    supports :status => true, :restart => true, "force-reload" => true
+    action [:enable, :start]
+  end
+
+end
