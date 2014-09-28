53dcbad8442217e5698b3c63fe8079fff3bd14bd
Manuel Bouza <manuel@bouza.ch>
2014-09-28 17:48:47 +0200

    removed enable from mysql service

--- cookbooks/mariadb/recipes/service.rb
+++ cookbooks/mariadb/recipes/service.rb
@@ -4,14 +4,14 @@ if (node[:mariadb][:is_first])
     supports :status => true, :restart => true, "force-reload" => true
     start_command "/etc/init.d/mysql start --wsrep-new-cluster"
     restart_command "/etc/init.d/mysql restart --wsrep-new-cluster"
-    action [:enable, :start]
+    action :start
   end
 
 else 
 
   service "mysql" do
     supports :status => true, :restart => true, "force-reload" => true
-    action [:enable, :start]
+    action :start
   end
 
 end
